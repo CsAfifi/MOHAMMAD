6d67082c79ebeab817983f392ef185581aae3204
CsAfifi <mohammadafifi360@gmail.com>
2024-11-21 18:07:57 +0300

    Add files via upload

---
+++ test.dart
@@ -0,0 +1,10 @@
+//prefixIcon: Icon(
+ //                             Icons.lock,
+   //                           color: Colors.purple[800],
+     //                       ),
+       //                     suffixIcon: IconButton(
+         //                       onPressed: () {
+           //                       setState(() {
+             //                       _obsecureText = !_obsecureText;
+               //                   });
+                 //               },
